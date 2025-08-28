340a0ce246c49798a237ed9c677b5fd8d0d37489
Anjali Prasad <95201486+AnjaliPrasad3@users.noreply.github.com>
2025-08-28 23:18:25 +0530

    Add files via upload

---
+++ src/main/java/com/example/GreenBill/GreenBillApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenBill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GreenBill/GreenBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenBill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
